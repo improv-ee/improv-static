fd116c15224d34d8b55e3bd5e98e54e21e703432
Ando Roots <ando@sqroot.eu>
2022-06-26 17:39:45 +0300

    Use non-hidden out dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    "nitro": {
+        "output": {
+            dir: 'output',
+            serverDir: 'output/server',
+            publicDir: 'output/public'
+        }
+    }
 })
