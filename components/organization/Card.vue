b45b22bd1be1b5613d227d65187bb079ea1c7adb
Ando Roots <ando@sqroot.eu>
2022-07-04 23:27:57 +0300

    Add organizations page

---
+++ components/organization/Card.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col">
+        <div class="card">
+             <img :src="imgUrl" class="card-img-top" :alt="name"/>
+            <div class="card-body">
+                <h5 class="card-title text-center">{{ name }}</h5>
+                <p class="card-text">
+                    <slot name="description"></slot>
+                </p>
+
+            </div>
+            <div class="card-footer text-center">
+    <a :href="link" target="_blank" class="btn col-12 btn-primary">{{ name }} veebileht</a>
+  </div>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    props: {
+        link: { type: String, required: true },
+        image: { type: String, required: true },
+        name: {type: String, required: true}
+  },
+  computed: {
+    imgUrl() {
+        return new URL(`../../assets/img/${this.image}`, import.meta.url);
+    }
+  }
+})
+
+</script>
--- pages/trupid.vue
+++ pages/trupid.vue
@@ -1,5 +1,69 @@
 <template>
-    <div>
- 
+
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <OrganizationCard name="Ruutu10" image="ruutu10.jpg" link="https://ruutu10.ee">
+            <template #description>
+                <p>Pidulikkus on tore ja tervitatav, aga veel rohkem meeldib meile vaba ja lõbus õhkkond.</p>
+                ​<p>Kujuta ette, et sa istud koos oma parimate sõpradega. Iga uus mõte, mis välja öeldakse on naljakam
+                    kui eelmine,
+                    samal ajal te kõik teadvustate endale “kui loll see on”. See on selline hetk, mida sa ei saa edasi
+                    rääkida.
+                    See on see klassikaline “sa oleksid pidanud seal olema” moment. </p>
+
+                ​<p>Me oleme Ruutu10 ja just sellise õhkkonna loomisele olemegi keskendunud. </p>
+                ​<p>Me oleme õppinud improkomöödiat Chicagos, Londonis, Barcelonas, Kopenhaagenis ja Tamperes oma
+                    ala parimate juhendajate käe all ning nüüd anname neid teadmisi edasi. Me töötame selle nimel,
+                    et tõsta kohalikku komöödiakvaliteeti. Meil on unistus, et ühel päeval on Eestis kool ja teater,
+                    mis keskendub improle ja annab inimestele võimaluse lõõgastuda ning naerda kogu südamest.</p>
+
+                <p class="lead text-center">Naer vabastab!</p>
+            </template>
+        </OrganizationCard>
+
+        <OrganizationCard name="Improteater IMPEERIUM" image="impeerium.jpg" link="https://improimpeerium.ee/">
+            <template #description>
+                <p>Improteater IMPEERIUM kasvas välja 2014. aastal esimesest professionaalsest improtrupist Eestis.
+                    Liikmed on kutselised näitlejad, kes lisaks IMPEERIUMile kuuluvad teiste Eesti teatrite
+                    draamatruppidesse või tegutsevad vabakutselistena erinevates teatri-, tele- ja filmiprojektides.
+                    Peale näitlejate on IMPEERIUMis ka andekad muusikud ning suurepärased valguskunstnikud, kes aitavad
+                    levitada improarmastust Eestis.</p>
+
+                <p>Impros ei tea keegi, mis juhtuma hakkab, kuid kõik on võimalik! Ladinakeelne sõna ’imperium’ tähendab
+                    võimu.
+                    Improteater IMPEERIUM soovib vallutada kõikide eestlaste südamed ning tekitada sellega
+                    maailmakaardile veel
+                    üks improrõõmust tulvil maa, kus inimesed väärtustavad koostööd ja loovust ning kus valitseb
+                    kujutluse võim.</p>
+            </template>
+        </OrganizationCard>
+
+        <OrganizationCard name="Ehatäht" image="eha.jpg" link="https://www.facebook.com/ehat2ht/">
+            <template #description>
+                <p>Improtrupp Ehatäht sai tuule tiibadesse 2016. aasta oktoobris Von Glehni
+                    Teatri improteatri kursusel. Trupi sünnile aitas kaasa kursuse juhendaja
+                    Mairi Tikerpalu (Improteater IMPEERIUM). Väga kiiresti leiti ühine
+                    hingamine ja hakati astuma samades kingades. Improtrenni teeme Von
+                    Glehni Teatris.</p>
+
+                <p>Improtruppi Ehatäht saab kutsuda esinema sünnipäevadele,
+                    koosviibimistele, firmaüritustele, koduhoovi jne.</p>
+
+                <p class="lead text-center">Ma ütlen EHA, ma ütlen TÄHT, ma ütlen EHATÄHT! Impromaailmas on kõik
+                    võimalik!</p>
+            </template>
+        </OrganizationCard>
+
+        <OrganizationCard name="Eesti Improteater" image="improteater.jpeg" link="http://www.improteater.ee/">
+            <template #description>
+                <p>2009. aastal haaras improteatri õppimise võimalusest kinni hulk noori TÜ Viljandi Kultuuriakadeemia
+                    lõpetajaid ja nendega koos panimegi kassi katusele: 2009. aasta 29. oktoobril läksime No99 Teatris
+                    esimest korda teksti, laule või samme õppimata lavale etendust mängima, lootes vaid iseendale ja oma
+                    võimele improviseerida.</p>
+                <p>Eesti Improteater loob igal etendusel uue maailma – uute tegelaste, uute suhete, uute laulude ja
+                    naljadega… tänaseks oleme seda teinud juba üle kahesaja korra. </p>
+            </template>
+        </OrganizationCard>
     </div>
+
 </template>
