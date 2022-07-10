1adc00d0939692061a295edb79d789466aefd358
Ando Roots <ando@sqroot.eu>
2022-07-10 10:29:53 +0300

    Add info prints to build script

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+echo "Starting nuxi build"
+
 npx nuxi generate
 
 # CloudFlare Pages can not handle paths beginning with a dot for deployment
 # (internal check for file existence fails)
 mv .output output
+
+echo "Finished building site, output is at output/public"
+ls -la output/public
