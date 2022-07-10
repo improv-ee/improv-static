a502387f00e8e38f92d4357f369401979d49eb08
Ando Roots <ando@sqroot.eu>
2022-07-10 10:55:17 +0300

    Add images to media

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ npx nuxi generate
 
 # CloudFlare Pages can not handle paths beginning with a dot for deployment
 # (internal check for file existence fails)
-mv .output output
+mv .output/public output
 
 echo "Finished building site, output is at output/public"
 ls -la output
