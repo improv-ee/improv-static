2aa401255b5f13cd1cc4bc337484abfbf732793a
Ando Roots <ando@sqroot.eu>
2022-09-13 11:56:26 +0300

    Add tallinn improv group

--- components/learn/Course.vue
+++ components/learn/Course.vue
@@ -1,11 +1,13 @@
 <template>
 <div>
+
+    <h2 class="display-5 mt-3 text-center">{{ name }}</h2>
     <a :href="link">
     <img :src="image" class="img-header rounded mt-4 mb-2"/>
     </a>
     <slot name="description"></slot>
     <a class="btn btn-primary col-8 offset-2 mb-5 mt-1" :href="link" target="_blank">
-        Loe edasi - {{ cta }}
+        {{ cta }}
     </a>
 </div>
 </template>
@@ -17,7 +19,8 @@ export default defineComponent({
     props: {
         link: { type: String, required: true },
         image: { type: String, required: true },
-        cta: {type: String, required: true}
+        cta: {type: String, required: true},
+        name: {type: String, required: true}
   }
 })
 
--- pages/oppimine.vue
+++ pages/oppimine.vue
@@ -6,8 +6,8 @@
         <p>Alustada saab teatrihariduseta ning igas vanuses - vaja on vaid avatud meelt ning veidi julgust.
         </p>
         
-        <LearnCourse image="/img/ruutu10-kool.jpeg" cta="Ruutu10 - sissejuhatus improsse"
-            link="https://ruutu10.ee/tootoad/">
+        <LearnCourse image="/img/ruutu10-kool.jpeg" cta="Loe edasi - Ruutu10 improkursused"
+            link="https://ruutu10.ee/tootoad/" name="Ruutu10 improõpe">
             <template #description>
                 <p>Alusta naeruküllast improteekonda, et vabastada loominguline potentsiaal ning ületada hirm esinemise
                     ja vigade tegemise ees! Meie töötubades on toetav ja sõbralik suhtumine ning kogenud juhendajad.
@@ -19,7 +19,8 @@
 
             </template>
         </LearnCourse>
-        <LearnCourse image="/img/rahel-otsa-kool.jpeg" cta="Rahel Otsa Improkool" link="https://improkool.ee/">
+        <LearnCourse image="/img/rahel-otsa-kool.jpeg" cta="Loe edasi - Rahel Otsa Improkool" 
+        name="Rahel Otsa Improkool" link="https://improkool.ee/">
             <template #description>
                 <p>Rahel Otsa Improkool on loodud, et pakkuda kvaliteetset ja mitmekülgset improõpet.
                     Kooli eesmärgiks on anda võimalus saada osa ennast arendavast huvitegevusest,
@@ -29,12 +30,22 @@
                     arenguks kui neile, kes soovivad tulevikus ka ise improetendusi andma hakata.</p>
             </template>
         </LearnCourse>
-        <LearnCourse image="/img/vgt-impro.jpeg" cta="Improkursused VGT-s" link="https://vgt.ee/kursused/">
+        <LearnCourse name="VGT improkursused"
+         image="/img/vgt-impro.jpeg" cta="Loe edasi - Improkursused VGT-s" link="https://vgt.ee/kursused/">
             <template #description>
                 <p>Von Glehni teater pakub valikus improkursuseid nii lastele kui täiskasvanutele, algajatele kui edasijõudnutele.
                     Kursuseid juhendavad improkogemusega näitlejad truppidest Ehatäht, Improteater Impeerium ning Eesti Improteater.
                 </p>
                 
+            </template>
+        </LearnCourse>
+        <LearnCourse name="Tallinn Improv Group"
+        image="/img/tallinn-english-improv.jpg" cta="Join a meetup" link="https://www.facebook.com/groups/1311238489388244">
+            <template #description>
+                <p>
+                    For an evening full of laughter, peculiar storylines and surreal adventures that we make up as we go, come join our weekly improv meetup! 😁😉🤣
+If you just want to meet new people, play some games, learn a bit about improv - you've found the right group!
+                </p>
             </template>
         </LearnCourse>
     </div>
