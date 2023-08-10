cb822a3da44952ff6d221e8824b03ce3743251aa
Ando Roots <ando@sqroot.eu>
2023-08-10 20:35:37 +0300

    Use default out dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,5 @@
 import { defineNuxtConfig } from 'nuxt/config'
-const path = require('path');
 export default defineNuxtConfig({
-    nitro: {
-        output: {
-          publicDir: path.join(__dirname, 'static')
-        }
-      },
     "css": [
         "~/assets/scss/main.scss"
     ],
