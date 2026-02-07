eaa1d960aff9800c83b589b4fb7b888b7aa4ca49
Ando Roots <ando@sqroot.eu>
2026-02-07 09:13:07 +0200

    Remove static group listing, point to improteatmik

--- pages/trupid.vue
+++ pages/trupid.vue
@@ -1,108 +1,16 @@
 <template>
     <div>
         <div class="row">
-            <p class="text-center mb-4">Vaata truppide ajalugu visuaalselt <NuxtLink to="/impropuu">Impropuu</NuxtLink>-lt.</p>
+
+            <p class="mb-4">Nimekirja ja tutvustuse improteatrit viljelevatest truppidest leiad <a
+                    href="https://teatmik.improv.ee/s/22aeb82e-7c69-41f3-a2cb-34371d4e9359"
+                    title="Improteatmik">Improteatmiku truppide
+                    ja teatrite sektsioonist</a>.</p>
+
+            <p class="mb-4">Vaata truppide ajalugu visuaalselt <NuxtLink to="/impropuu">Impropuu</NuxtLink>
+                -lt.</p>
         </div>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
 
-            <OrganizationCard name="Ruutu10" image="/img/ruutu10.jpg" link="https://ruutu10.ee">
-                <template #description>
-                    <p>Pidulikkus on tore ja tervitatav, aga veel rohkem meeldib meile vaba ja lõbus õhkkond.</p>
-                    <p>Kujuta ette, et sa istud koos oma parimate sõpradega. Iga uus mõte, mis välja öeldakse on
-                        naljakam
-                        kui eelmine,
-                        samal ajal te kõik teadvustate endale “kui loll see on”. See on selline hetk, mida sa ei saa
-                        edasi
-                        rääkida.
-                        See on see klassikaline “sa oleksid pidanud seal olema” moment. </p>
-
-                    <p>Me oleme Ruutu10 ja just sellise õhkkonna loomisele olemegi keskendunud. </p>
-                    <p>Me oleme õppinud improkomöödiat Chicagos, Londonis, Barcelonas, Kopenhaagenis ja Tamperes oma
-                        ala parimate juhendajate käe all ning nüüd anname neid teadmisi edasi. Me töötame selle nimel,
-                        et tõsta kohalikku komöödiakvaliteeti. Meil on unistus, et ühel päeval on Eestis kool ja teater,
-                        mis keskendub improle ja annab inimestele võimaluse lõõgastuda ning naerda kogu südamest.</p>
-
-                    <p class="lead text-center">Naer vabastab!</p>
-                </template>
-            </OrganizationCard>
-
-            <OrganizationCard name="Improteater IMPEERIUM" image="/img/impeerium.jpg" link="https://improimpeerium.ee/">
-                <template #description>
-                    <p>Improteater IMPEERIUM kasvas välja 2014. aastal esimesest professionaalsest improtrupist Eestis.
-                        Liikmed on kutselised näitlejad, kes lisaks IMPEERIUMile kuuluvad teiste Eesti teatrite
-                        draamatruppidesse või tegutsevad vabakutselistena erinevates teatri-, tele- ja filmiprojektides.
-                        Peale näitlejate on IMPEERIUMis ka andekad muusikud ning suurepärased valguskunstnikud, kes
-                        aitavad
-                        levitada improarmastust Eestis.</p>
-
-                    <p>Impros ei tea keegi, mis juhtuma hakkab, kuid kõik on võimalik! Ladinakeelne sõna ’imperium’
-                        tähendab
-                        võimu.
-                        Improteater IMPEERIUM soovib vallutada kõikide eestlaste südamed ning tekitada sellega
-                        maailmakaardile veel
-                        üks improrõõmust tulvil maa, kus inimesed väärtustavad koostööd ja loovust ning kus valitseb</p>
-                    <p class="lead text-center">kujutluse võim.</p>
-                </template>
-            </OrganizationCard>
-
-            <OrganizationCard name="Ehatäht" image="/img/eha.jpg" link="https://www.facebook.com/ehat2ht/">
-                <template #description>
-                    <p>Improtrupp Ehatäht sai tuule tiibadesse 2016. aasta oktoobris Von Glehni
-                        Teatri improteatri kursusel. Trupi sünnile aitas kaasa kursuse juhendaja
-                        Mairi Tikerpalu (Improteater IMPEERIUM). Väga kiiresti leiti ühine
-                        hingamine ja hakati astuma samades kingades. Improtrenni teeme Von
-                        Glehni Teatris.</p>
-
-                    <p>Improtruppi Ehatäht saab kutsuda esinema sünnipäevadele,
-                        koosviibimistele, firmaüritustele, koduhoovi jne.</p>
-
-                    <p class="lead text-center">Ma ütlen EHA, ma ütlen TÄHT, ma ütlen EHATÄHT! Impromaailmas on kõik
-                        võimalik!</p>
-                </template>
-            </OrganizationCard>
-
-            <OrganizationCard name="Eesti Improteater" image="/img/improteater.jpg" link="http://www.improteater.ee/">
-                <template #description>
-                    <p>2009. aastal haaras improteatri õppimise võimalusest kinni hulk noori TÜ Viljandi
-                        Kultuuriakadeemia
-                        lõpetajaid ja nendega koos panimegi kassi katusele: 2009. aasta 29. oktoobril läksime No99
-                        Teatris
-                        esimest korda teksti, laule või samme õppimata lavale etendust mängima, lootes vaid iseendale ja
-                        oma
-                        võimele improviseerida.</p>
-                    <p>Eesti Improteater loob igal etendusel uue maailma – uute tegelaste, uute suhete, uute laulude ja
-                        naljadega… tänaseks oleme seda teinud juba üle kahesaja korra. </p>
-                </template>
-            </OrganizationCard>
-            <OrganizationCard name="Kahe Vahel" image="/img/kahe-vahel.jpg" link="https://ruutu10.ee/">
-                <template #description>
-                    <p>Nad on kaks sõpra, Märt ja Kristjan, kelle koostöö on kestnud juba mitu hooaega. Kahekesi laval
-                        hakkama saamine on väljakutse, mille nad alati rõõmuga vastu võtavad ja millega meisterlikult
-                        toime
-                        tulevad. Nad pakuvad hulgaliselt spontaansust, äratundmisrõõmu ja pisarateni liigutavaid
-                        suhtesituatsioone. </p>
-
-                    <p>
-                        Märt ja Kristjan küsivad publikuliimetelt muusikapala, millest saab nende etteaste intro. Nad
-                        inspireeruvad muusika emotsioonist ning seejärel improviseerivad kuni 30 minuti pikkuse
-                        näidendi.
-                        Raske on ette ennustada, millest nende lugu räägib, kuid seni on need olnud südamlikud,
-                        koomilised
-                        ja kaasahaaravad.</p>
-                </template>
-            </OrganizationCard>
-            <OrganizationCard name="kIMPROose" image="/img/kimproose.jpg" link="https://www.facebook.com/kimproose">
-                <template #description>
-                    <p>Improtrupp kIMPROose on justkui värske sõõm naerugaasi. Aastaid koos käinud trupp toimib laval
-                        ühe meeskonnana ning eesmärk on kõigil sama - lõbus peab olema.
-                        Seda nii vaatajatel, kui ka trupiliikmeil endil.</p>
-
-                    <p>kIMPROose etteasted on lustakad, muusikalised, krutskitega ning täis erinevaid põnevaid pöördeid.
-                        Armastame avastada laval uusi suhteid ja tegelasi ning vaadata, kuhu see kõik lõpuks kokku viib.
-                    </p>
-                </template>
-            </OrganizationCard>
-        </div>
     </div>
 </template>
 <script setup>
